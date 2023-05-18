4586e07f6ecdb04aebe699ddbf28130474cd6cd2
DevExpressExampleBot <fakeEmail>
2023-05-18 17:56:35 +0400

    Source auto update [skip ci]

--- VB/PreviewAndPrint/Form1.vb
+++ VB/PreviewAndPrint/Form1.vb
@@ -1,32 +1,30 @@
 Imports DevExpress.Drawing.Printing
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 
 Namespace PreviewAndPrint
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
+
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' set advanced print options, if needed
             chartControl1.OptionsPrint.SizeMode = DevExpress.XtraCharts.Printing.PrintSizeMode.Zoom
             printableComponentLink1.Landscape = True
             printableComponentLink1.PaperKind = DXPaperKind.A4
         End Sub
 
-        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
             printableComponentLink1.ShowPreview()
         End Sub
 
-        Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton2.Click
+        Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As EventArgs)
             printableComponentLink1.Print(String.Empty)
         End Sub
     End Class
